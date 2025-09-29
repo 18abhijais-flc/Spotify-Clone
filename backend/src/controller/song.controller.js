8db33e9bf40e02f85c71a5b17eb191b34bedc05a
Abhishek <abhishekjaiswal1964@gmail.com>
2025-09-29 23:53:06 +0530

    Adding user controller in js

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -34,7 +34,48 @@ try {
 };
 
 export const getTrendingSongs = async (req, res, next) => {
+     try {
+    const songs = await Song.aggregate([
+        {
+        $sample: {size: 4 },
+        },
+        {
+            $project: {
+                _id: 1,
+                title: 1,
+                artist: 1,
+                imageUrl: 1,
+                audioUrl: 1,
+                audioUrl: 1,
+            },
+        },
+    ]);
+    res.json(songs);
+    } catch (error) {
+    next(error);
+}
 };
 
 
-export const getMadeForYouSongs= async (req, res, next) => {};
+export const getMadeForYouSongs= async (req, res, next) => {
+    try {
+    const songs = await Song.aggregate([
+        {
+        $sample: {size: 4 },
+        },
+        {
+            $project: {
+                _id: 1,
+                title: 1,
+                artist: 1,
+                imageUrl: 1,
+                audioUrl: 1,
+                audioUrl: 1,
+            },
+        },
+    ]);
+    res.json(songs);
+    } catch (error) {
+    next(error);
+}
+};
